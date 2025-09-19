9788f416fe0f68413ed01125d6173e78957a513f
cheolwooklee <lcw5745187@mleverage.co.kr>
2025-09-19 14:32:53 +0900

    feat: modal content height width props 추가

--- Modal/components/Content/Content.tsx
+++ Modal/components/Content/Content.tsx
@@ -2,12 +2,17 @@ import React, { useRef } from 'react';
 import { useModal } from '../../Modal';
 import styles from './Content.module.scss';
 import classNames from 'classnames';
+import type { CSSVariables } from '@/shared/types/css/CSSVariables';
+import { toCssUnit } from '@/shared/utils';
 type ContentProps = React.HTMLAttributes<HTMLDivElement> & {
     children: React.ReactNode;
+    width?: string | number;
+    height?: string | number;
 };
 
-export const Content = ({ children, ...props }: ContentProps) => {
+export const Content = ({ width = 'auto', height = 'auto', children, style, ...props }: ContentProps) => {
     const { modalValue } = useModal();
+
     const containerRef = useRef<HTMLDivElement>(null);
 
     const combinedStyle = classNames(props.className, styles.Content, {
@@ -15,9 +20,14 @@ export const Content = ({ children, ...props }: ContentProps) => {
         [styles.Closed]: !modalValue, // dropdownValue가 false일 때 Closed 클래스 적용
     });
 
+    const cssVariables: CSSVariables = {
+        '--width': toCssUnit(width),
+        '--height': toCssUnit(height),
+    };
+
     return (
         <>
-            <div ref={containerRef} {...props} className={combinedStyle}>
+            <div ref={containerRef} {...props} className={combinedStyle} style={{ ...cssVariables, ...style }}>
                 {children}
             </div>
         </>
