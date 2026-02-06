9dd4b540822819ed6804d6eef017148dd21694a3
mlevearge-dhkim <dhkim@mleverage.co.kr>
2026-02-06 11:06:33 +0900

    fix: Modal, Dropdown.Content 수정 - z-index 이슈 해결

--- Modal/components/Content/Content.tsx
+++ Modal/components/Content/Content.tsx
@@ -36,7 +36,13 @@ export const Content = ({
 
     return (
         <>
-            <div ref={containerRef} {...props} className={combinedStyle} style={{ ...cssVariables, ...style }}>
+            <div
+                ref={containerRef}
+                {...props}
+                data-modal-content="true"
+                className={combinedStyle}
+                style={{ ...cssVariables, ...style }}
+            >
                 {children}
             </div>
         </>
